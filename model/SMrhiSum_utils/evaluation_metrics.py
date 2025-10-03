aed7ec3df206af12b2751b4bd5b02a679319d751
charazerva <charazerva01@gmail.com>
2025-10-03 11:05:49 +0300

    SD-MVSum: full project release

---
+++ model/SMrhiSum_utils/evaluation_metrics.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+import numpy as np
+from scipy import stats
+
+
+def evaluate_summary(predicted_summary, user_summary, score, gtscore, eval_method='avg'):
+    """ Compare the predicted summary with the user defined one(s).
+    :param ndarray predicted_summary: The generated summary from our model.
+    :param ndarray gt_summary: The user defined ground truth summaries (or summary).
+    """
+
+    # evaluation method for summe and tvsum dataset also applicable on yt8m
+    max_len = max(len(predicted_summary), user_summary.shape[0])
+    S = np.zeros(max_len, dtype=int)
+    G = np.zeros(max_len, dtype=int)
+    S[:len(predicted_summary)] = predicted_summary
+    f_scores = []
+    for user in range(1):  # user_summary.shape[0]
+        G[:user_summary.shape[0]] = user_summary
+        overlapped = S & G
+        # Compute precision, recall, f-score
+        precision = sum(overlapped) / sum(S + 1e-8)
+        recall = sum(overlapped) / sum(G + 1e-8)
+
+        if precision + recall == 0:
+            f_scores.append(0)
+        else:
+            f_scores.append((2 * precision * recall * 100) / (precision + recall))
+
+    if eval_method == 'max':
+        f_score_result = max(f_scores)
+    else:
+        f_score_result = sum(f_scores) / len(f_scores)
+
+    y_pred2 = score.numpy()
+    y_true2 = gtscore.numpy()
+    pS = stats.spearmanr(y_pred2, y_true2)[0]
+    kT = stats.kendalltau(stats.rankdata(np.array(y_pred2)), stats.rankdata(np.array(y_true2)))[0]
+    return f_score_result, kT, pS
---
+++ model/data_loader.py
@@ -0,0 +1,65 @@
+# -*- coding: utf-8 -*-
+import torch
+import h5py
+import json
+from torch.utils.data import Dataset, DataLoader
+
+class VideoData(Dataset):
+    def __init__(self, mode, dataset='S_VideoXum'):
+        self.mode = mode
+        self.dataset = dataset
+
+        if dataset == 'S_VideoXum':
+            self.filename = './dataset/script_videoxum.h5'
+            self.dataset_split = './dataset/script_videoxum_split.json'
+        elif dataset == 'S_MrhiSum':
+            self.filename = './dataset/script_mrhisum.h5'
+            self.dataset_split = './dataset/script_mrhisum_split.json'
+        else:
+            raise ValueError("Invalid dataset.")
+
+        self.hdf = h5py.File(self.filename, 'r')
+        with open(self.dataset_split, 'r') as f:
+            keys = json.load(f)
+
+        self.video_names = [v for v in self.hdf.keys() if v in keys[self.mode]]
+
+    def __len__(self):
+        return len(self.video_names)
+
+    def __getitem__(self, index):
+        video_name = self.video_names[index]
+
+        with h5py.File(self.filename, 'r') as hdf:
+            video_features = torch.from_numpy(hdf[video_name + '/video_embeddings'][()]).float()
+            text_features = torch.from_numpy(hdf[video_name + '/text_embeddings'][()]).float()
+            gtscore = torch.from_numpy(hdf[video_name + '/gtscores'][()]).float()
+            transcripts = torch.from_numpy(hdf[video_name + '/aligned_transcripts'][()]).float()
+
+            if self.dataset == 'S_MrhiSum':
+                cps = torch.from_numpy(hdf[video_name + '/change_points'][()]).float()
+                gt_summary = torch.from_numpy(hdf[video_name + '/gt_summary'][()]).float()
+                return video_features, text_features, transcripts, gtscore, cps, gt_summary
+            else:
+                return video_features, text_features, transcripts, gtscore
+
+
+
+def get_loader(mode, dataset='S_VideoXum'):
+    """ Loads the dataset.
+    Wrapped by a Dataloader, shuffled and `batch_size` = 1 in train `mode`.
+
+    :param str mode: The mode of the model, train or test.
+    :param str dataset: The name of the dataset: ['S_VideoXum' | 'S_NewsVSum']
+    :return: The dataset used in each mode.
+    """
+    vd = VideoData(mode, dataset=dataset)
+
+    if mode.lower() == 'train':
+        return DataLoader(vd, batch_size=1, shuffle=True)
+    else:
+        return DataLoader(vd, batch_size=1, shuffle=False)
+
+
+if __name__ == '__main__':
+    pass
